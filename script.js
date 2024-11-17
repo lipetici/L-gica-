c9f5b09c6fba1b9c5f8548ca7272a0184e3bde9c
lipetici <fellipeandre160@gmail.com>
2024-11-16 21:44:08 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
-// função para geraar um número aleatóro
-alert ('boas vindas ao jogo do número secreto');
+// Função para gerar um número aleatório 
+
+alert('Boas vindas ao jogo do número secreto!');
 
 const numeroMaximo = 100;
 const nivelFacil = 20;
 const nivelMedio = 10;
-const nivelDificil = 5;
+const nivelDificil= 5;
 
-let numeroSecreto = Math.randon();
+
+let numeroSecreto = Math.random();
 let chute;
 let tentativas = 0;
 
 function gerarNumeroAleatorio() {
-    let numero = 0;
-    numero = Math.floor(numeroSecreto * numeroMaximo) + 1;
+let numero = 0;
+  numero = Math.floor(numeroSecreto * numeroMaximo) + 1;
 
-    return numero;
-    }
-
-    numeroSecreto = gerarNumeroAleatorio();
-
-    /*
-    if = Se
-    Else = Senão
-    while = Enquanto
-    */
-   while (chute != numeroSecreto) {
-    tentativas++;
-    if(tentativas<= nivelMedio) {
-chute = prompt(`Escolha um numero de 1 a ${numeroMaximo}:`);
-
-if (chute == numeroSecreto) {
-    alert(`Parabéns ! voce acertou o número secreto ${numeroSecreto}
-        // com ${tentativas} ${tentativas == 1 ? 'tentativa' : 'tentativa'}.`);
-} else { 
-alert(`voce errou! \n\nNúmero de tentativa(s): ${tentativas} de ${nivelMedio}`);
-
-if (numeroSecreto > chute) {
-    alert(`O número secreto é maior ${chute}.`)
-} else {
-    alert(` O numero secreto é menor ${chute}.`)
+return numero;
 }
-}
+
+numeroSecreto = gerarNumeroAleatorio();
+
+
+
+while (chute != numeroSecreto) {
+  tentativas++;
+if (tentativas <= nivelMedio){
+  chute = prompt('Escolha um número de 1 a 100: ');
+
+  if (chute == numeroSecreto) {
+    alert(`Parabéns!!! Você acertou o número secreto ${numeroSecreto} com ${tentativas} ${tentativas == 1 ? 'tentativa' : 'tentativas'}.`);
+  } else {
+    alert(`Você Errou!!!\n\nNúmero de tentativa(s): ${tentativas} de ${nivelMedio}`);
+    if (numeroSecreto > chute) {
+      alert(`O número secreto é maior que ${chute}.`);
     } else {
-        alert('Game Over:\nVoce perdeu!\n\nTente novamente.');
-        break; 
-   }
-   }
+      alert(`O número secreto é menor que ${chute}.`);
+    }
+  }
+}else {
+  alert('Game Over:\nVocê perdeu!!\n\nTente novamente.');
+  break;
+}
+}
