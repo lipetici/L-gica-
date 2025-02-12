79ed6a3bbf6cf5991bafe58f94293a9675191c01
lipetici <fellipeandre160@gmail.com>
2025-02-12 19:21:26 -0300

    192112022025

--- script.js
+++ script.js
@@ -31,8 +31,49 @@ Switch Case = é uma construção condicionall que permite que um bloco de códi
 */
 
 while (!nivelValido.includes(nivel)) {
-    nivel = parseInt(prompt(
+    nivel = parseInt(prompt(`Escolha o nível do jogo:
+        1 - nivel fácil: ${nivelFacil} tentativas
+        2 - nivel Médio: ${nivelMedio} tentativas
+        3 - nivel Difícil: ${nivelDificil} tentativas `));
 
-        
-    ))
+        switch (parseInt(nivel)) {
+            case 1:
+                nivelEscolhido = nivelFacil;
+                break;
+                case 2:
+                    nivelEscolhido = nivelMedio;
+                    break;
+                    case 3:
+                        nivelEscolhido = nivelDificil;
+                        break;
+                        default:
+                            alert('nivel informado incorreto!\nInforme um nivel de 1 a 3.')
+        }
+};
+
+while (chute != numeroSecreto) {
+    tentativas++;
+    if (tentativas<= nivelEscolhido) {
+        chute = prompt(`Escolha um numero de 1 a ${numeroMaximo}: `);
+
+        if (chute == numeroSecreto) {
+            alert(`Parabéns Voce acertou o numero secreto ${numeroSecreto}
+                com ${tentativas} ${tentativas == 1 ? 'tentativa' : 'tentativas'}.`)
+        } else {
+            alert(`Voce Errou !!!\n\nNúmero de tentativas(s): ${tentativas} de ${nivelEscolhido}`);
+
+if (tentativas < nivelEscolhido) {
+    if (numeroSecreto > chute) {
+        alert(`O número secreto é maior ${chute}.`);
+    } else {
+        alert(`O número secreto é menor ${chute}.`);
+    }
 }
+}
+} else {
+    alert(`Game Over:\nVoce perdeu !!!\n\nTente novamente
+        \n\nO número secreto era: ${numeroSecreto}.`);
+        break;
+}
+}
+
